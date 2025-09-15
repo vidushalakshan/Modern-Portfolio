edb790ac4d6a90ea0c32a15f88d087354e5d37f9
vidushalakshan <vidushalakshan7890@gmail.com>
2025-09-15 21:28:25 +0530

    updated view in home page

--- src/components/pages/Home/page.jsx
+++ src/components/pages/Home/page.jsx
@@ -7,12 +7,12 @@ const page = () => {
     <section className="text-black relative h-screen bg-[#e8e8e3]">
       <Image
         src={Images.homeImg}
-        className="absolute right-0 bottom-0 object-cover w-[500] h-screen"
+        className="absolute right-0 bottom-0 object-cover rounded-t-full w-[350]"
       />
-      <div className="flex flex-col gap-5 absolute top-1/6 left-10">
+      <div className="flex flex-col absolute top-1/6 left-10">
         <h1 className="text-[150px] font-bold">VIDUSHA LAKSHAN</h1>
-        <LuArrowDownRight />
-        <span>
+        <LuArrowDownRight size={50} className="relative" color="#b6b6b0"/>
+        <span className="text-[25px] mt-5 w-[600px]">
           Open to job opportunities world wide.Passionate about building
           polished,intuitive,and thoughtful digital experiences that leave
           amark.
