fd7787127f975b02bde6d2e35faf027abfa13374
vidushalakshan <vidushalakshan7890@gmail.com>
2025-09-15 20:51:58 +0530

    updaed home main name

--- src/components/pages/Home/page.jsx
+++ src/components/pages/Home/page.jsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import { LuArrowDownRight } from "react-icons/lu";
-import Images from "../../../constants/images"
+import Images from "../../../constants/images";
 
 const page = () => {
   return (
-    <section className="text-white relative h-screen">
-      <Image src={Images.homeImg} className="absolute right-0 bottom-0 object-cover w-[500] h-screen"/>
-      <div>
-        <h1>VIDUSHA LAKSHAN</h1>
-      </div>
-      <div>
+    <section className="text-black relative h-screen bg-[#e8e8e3]">
+      <Image
+        src={Images.homeImg}
+        className="absolute right-0 bottom-0 object-cover w-[500] h-screen"
+      />
+      <div className="flex flex-col gap-5 absolute top-1/6 left-10">
+        <h1 className="text-[150px] font-bold">VIDUSHA LAKSHAN</h1>
         <LuArrowDownRight />
-      </div>
-      <div>
-        <span>Open to job opportunities world wide.Passionate about building polished,intuitive,and thoughtful digital experiences that leave amark.</span>
+        <span>
+          Open to job opportunities world wide.Passionate about building
+          polished,intuitive,and thoughtful digital experiences that leave
+          amark.
+        </span>
       </div>
     </section>
   );
