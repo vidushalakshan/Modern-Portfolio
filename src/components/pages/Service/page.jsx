320dafd79d3fd9386e88747bc23d606e5b9b2daa
vidushalakshan <vidushalakshan7890@gmail.com>
2025-09-19 14:42:04 +0530

    updated top service section

--- src/components/pages/Service/page.jsx
+++ src/components/pages/Service/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <section className='mt-20'>
-      <h1 className='uppercase'>What i do /</h1>
-    </section>
-  )
-}
+    <section className="mt-20 px-6 sm:px-12 relative">
+      {/* Left side heading */}
+      <h1 className="uppercase text-[clamp(3rem,10vw,8rem)] font-bold text-[#e8e8e3] leading-tight">
+        What I Do /
+      </h1>
 
-export default page
+      {/* Right side content */}
+      <div className="mt-20 flex sm:absolute sm:right-42 sm:top-24 max-w-[700px] gap-6">
+        <span className="uppercase text-[16px] mt-2 text-gray-400">
+          (Services)
+        </span>
+        <p className="text-[#e8e8e3] text-[23px] leading-relaxed">
+          I specialize in building full-stack web applications that are fast,
+          reliable, and user-friendly. With a solid foundation in both frontend
+          and backend technologies, I help bring ideas to life whether it’s for
+          a business, a startup, or a product team.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
