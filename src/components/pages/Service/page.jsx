be9586e50c03045dace6bd6fa7cc642f417b7546
vidushalakshan <vidushalakshan7890@gmail.com>
2025-09-21 19:48:57 +0530

    updated service top section responive max sm size

--- src/components/pages/Service/page.jsx
+++ src/components/pages/Service/page.jsx
@@ -88,11 +88,11 @@ const Service = () => {
       </h1>
 
       {/* Right side content */}
-      <div className="mt-20 flex sm:absolute sm:right-42 sm:top-24 max-w-[700px] mb-[300px] gap-6 relative">
-        <span className="uppercase text-[16px] mt-2 text-gray-400">
+      <div className="mt-20 max-sm:mt-10 flex sm:absolute sm:right-42 sm:top-24 max-w-[700px] mb-[300px] gap-6 relative">
+        <span className="uppercase text-[16px] mt-2 text-gray-400 max-sm:text-[12px]">
           (Services)
         </span>
-        <p className="text-[#e8e8e3] text-[23px] leading-relaxed">
+        <p className="text-[#e8e8e3] text-[23px] leading-relaxed max-sm:text-[12px]">
           I specialize in building full-stack web applications that are fast,
           reliable, and user-friendly. With a solid foundation in both frontend
           and backend technologies, I help bring ideas to life whether it’s for
